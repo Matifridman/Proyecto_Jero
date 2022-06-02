5cebaa4a2c63d546d8f3ad800a96b524de1d1ce1
Matifridman <82188442+Matifridman@users.noreply.github.com>
2022-06-02 09:37:25 -0300

    Initial commit

---
+++ Assets/scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed;
+    }
+
+    void OnCollisionEnter()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/scripts/Gun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletSpawn = null;
+    public float reloadTime;
+    float currReloadTime;
+    bool canShoot = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currReloadTime > 0)
+        {
+            currReloadTime -= Time.deltaTime;
+        }
+
+        if (Input.GetMouseButtonDown(0) && currReloadTime <= 0)
+        {
+            Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+            currReloadTime = reloadTime;
+        }
+    }
+}
