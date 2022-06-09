9781f9cd8794c55b0b9cc6f5c7bad2025b8eeb13
Matifridman <82188442+Matifridman@users.noreply.github.com>
2022-06-09 15:48:15 -0300

    Teleport&CheckPoint

---
+++ Assets/scripts/Teleport.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    /*public GameObject Player;
+    public string teleport;
+    private Vector3 Spawnpoint¨*/
+
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+    void OnTriggerEnter( Collider other)
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;
+    }
+    
+    void Start()
+    {
+        //Spawnpoint = new Vector3(1.3f, 20f, 59.8f);
+    }
+
+    
+    void Update()
+    {
+
+    }
+    /*void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Teleport")
+        {
+            //Instantiate(Player, Spawnpoint, Quaternion.identity);
+            Player.transform.position += Spawnpoint;
+        }
+    }*/
+}
