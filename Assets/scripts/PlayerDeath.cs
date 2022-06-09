2cd212729ec11a18cd1db45efe07af9fed543c12
Matifridman <82188442+Matifridman@users.noreply.github.com>
2022-06-09 11:56:31 -0300

    SpawnPoint(Finished)

---
+++ Assets/scripts/PlayerDeath.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public GameObject Player;
+    public string Enemy;
+    private Vector3 SpawnPoint;
+
+    private float Speed = 0.1f;
+    private float Rotation = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPoint = new Vector3(0, 0.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, Speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -Speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Rotation,0 , 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Rotation, 0, 0);
+        }
+
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == Enemy)
+        {
+            Instantiate(Player, SpawnPoint, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/SpawnPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject Player;
+    public string Death_point;
+    private Vector3 Spawnpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawnpoint = new Vector3(-38.1f, 6f, 3.4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == Death_point)
+        {
+            Instantiate(Player, Spawnpoint, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
